cb2229234de62590ed3d88b4eefd160975173a68
guyujia98 <guyujia98@163.com>
2023-06-02 18:17:21 +0800

    change package description

--- R/caratMULT-package.R
+++ R/caratMULT-package.R
@@ -3,7 +3,9 @@
 #' \packageIndices{caratMULT}
 #' @author
 #' \packageAuthor{caratMULT}
+#'  
 #'  Maintainer: \packageMaintainer{caratMULT}
+#' 
 #' @references Gu Y., Liu H., Ma W.(2023) \emph{Regression-based multiple treatment effect estimation under covariate-adaptive 
 #' randomization.} In review.
 #' 
